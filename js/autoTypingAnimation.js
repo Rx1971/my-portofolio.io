28ad9d5d82c3f6dd9865442f17dc3271d994b004
Rx1971 <rahman787887@gmail.com>
2023-01-06 22:04:17 +0700

    Portofolio Pertama Saya

---
+++ js/autoTypingAnimation.js
@@ -0,0 +1,45 @@
+// setTimeout(function() {
+//     $('.about-info-inner').typed({
+//       strings: [
+//         "<span>Nama Lengkap : ABDUL RAHMAN</span>"
+//       ],
+//       typeSpeed: 6,
+//       contentType: 'html'
+//     });
+//   }, 100);
+//   setTimeout(function() {
+//     $('.about-info2-inner').typed({
+//       strings: [
+//         "<span>Umur : 20 Tahun</span>"
+//       ],
+//       typeSpeed: 6,
+//       contentType: 'html'
+//     });
+//   }, 100);
+//   setTimeout(function() {
+//     $('.about-info3-inner').typed({
+//       strings: [
+//         "<span> Assalamualaikum Warahmatullahi Wabarakatuh</span>"
+//       ],
+//       typeSpeed: 6,
+//       contentType: 'html'
+//     });
+//   }, 100);
+//   setTimeout(function() {
+//     $('.about-info4-inner').typed({
+//       strings: [
+//         "<span> Assalamualaikum Warahmatullahi Wabarakatuh</span>"
+//       ],
+//       typeSpeed: 6,
+//       contentType: 'html'
+//     });
+//   }, 100);
+//   setTimeout(function() {
+//     $('.about-info5-inner').typed({
+//       strings: [
+//         "<span> Assalamualaikum Warahmatullahi Wabarakatuh</span>"
+//       ],
+//       typeSpeed: 6,
+//       contentType: 'html'
+//     });
+//   }, 100);
